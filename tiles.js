c3109290ebfe4d4c37cab8c1d3497aee53ecc40a
Sarah Walters <sarah.walters@students.olin.edu>
2014-08-11 13:34:28 -0400

    New .txt access structure

--- tiles.js
+++ tiles.js
@@ -13,27 +13,20 @@ var CONTENT = {
 	},
 
 	makeTiles: function() {
-		/*$.ajax({
-            url : "tiles/01/text.txt",
-            dataType: "text",
-            success : function (data) {
-               	console.log(data);
-            }
-        });*/
-
-		var title;
+		/*var title;
 		var text;
 		var imgs;
 		$.get('tiles/01/text.txt', function(data) {
 			var a = data.split('\n***\n');
-			console.log(a);
 			title = a[0]
 			text = a[1]
 			imgs = a[2].split('\n');
 			console.log(title)
 			console.log(text)
 			console.log(imgs)
-		});
+		});*/
+
+		CONTENT.buildTile('01');
 
 		if (CONTENT.tiles == null) {
 			var ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis ultricies nibh a volutpat. Cras aliquet, turpis id viverra fringilla, felis arcu placerat lectus, vitae venenatis tellus nunc eget elit. Vestibulum vitae leo bibendum, euismod ligula a, pellentesque mi. Quisque in elit lectus. Sed pellentesque elementum nunc, non cursus tellus venenatis vel. Nullam ac faucibus arcu. Sed enim tellus, iaculis a nunc non, sodales fringilla elit. Morbi interdum lobortis ultrices. Nam fermentum lorem enim, a tincidunt est pellentesque at. Ut in interdum velit, vel malesuada est.';
@@ -61,6 +54,20 @@ var CONTENT = {
 			$(UTILS.colIds[shortestColIndex]).append(tRender);
 			colHeights[shortestColIndex] += $('#tile-'+t.id).height();
 		});
+	},
+
+	buildTile: function(num) {
+		$.get('tiles/'+num+'/text.txt', function(data) {
+			var a = data.split('\n***\n');
+			console.log(a);
+			makeTileObject(a[0], a[1], a[2].split('\n'));
+		});
+	},
+
+	makeTileObject: function(title, text, imgs) {
+		console.log(title)
+		console.log(text)
+		console.log(imgs)
 	},
 
 	filterTiles: function() {
