8858dbbe9a15581e89e8366b148e2f7f5b784323
Sarah Walters <swalters4925@gmail.com>
2016-12-25 04:14:55 -0500

    ADD: mobile breakpoint

--- src/js/layout.js
+++ src/js/layout.js
@@ -7,10 +7,16 @@ var LAYOUT = (function() {
 		// compute widths based on page & main widths
 		// breakpoint at 1100 (photo on left vs. photo above)
 		if (windowWidth > 1100) {
+			// widescreen -- photo on left
 			leftWidth = 0.275 * mainWidth;
 			rightWidth = mainWidth - leftWidth;
-		} else {
+		} else if (windowWidth > 500) {
+			// halfscreen -- photo above
 			leftWidth = 0.3 * windowWidth;
+			rightWidth = windowWidth;
+		} else {
+			// mobile screen -- photo larger
+			leftWidth = 0.4 * windowWidth
 			rightWidth = windowWidth;
 		}
 
